6d5b87b01f3403c877aa10eaf8de55e55293f4cf
TheRAlex <acumanaosorio@byupathway.edu>
2026-05-13 19:42:09 -0400

    final changes

--- Week02/scripts/bom.js
+++ Week02/scripts/bom.js
@@ -1,12 +1,12 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('_____');
+const list = document.querySelector('#list');
 
 const li = document.createElement('li');
 
 const deleteButton = document.createElement('button');
 
-li.textContent = input.value;
+ li.textContent = input.value;
 
 deleteButton.textContent = '✖️'
 
